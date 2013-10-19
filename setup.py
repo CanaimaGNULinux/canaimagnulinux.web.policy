7c1211a171d1f46a0b50b85430e833b9a7be79a6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2013-10-18 19:30:05 -0430

    Actualizar metada del paquete

--- setup.py
+++ setup.py
@@ -5,20 +5,29 @@ version = '0.1'
 
 setup(name='canaimagnulinux.web.policy',
       version=version,
-      description="A Plone 3 policy product for customizing a Plone site",
+      description="A Plone 4.3 policy product for customizing a Plone site",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Office/Business :: Groupware",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='plone3 policy product canaima gnu linux website',
+      keywords='plone4 policy product canaima gnu linux website',
       author='Leonardo J. Caballero G.',
       author_email='leonardocaballero@gmail.com',
-      url='https://github.com/canaimagnulinux/canaimagnulinux.web.policy',
-      license='GPL',
+      url='https://github.com/CanaimaGNULinux/canaimagnulinux.web.policy',
+      license='GPLv2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['canaimagnulinux', 'canaimagnulinux.web'],
       include_package_data=True,

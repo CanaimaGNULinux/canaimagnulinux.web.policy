da6e409fe03dd6e563a18a735f49f461cf4123b3
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2014-07-01 03:14:27 -0430

    Added canaima, soluciom y comunidad folders tests

--- canaimagnulinux/web/policy/tests/test_site_structure.py
+++ canaimagnulinux/web/policy/tests/test_site_structure.py
@@ -6,10 +6,7 @@ This is an integration "unit" test for Site Structure
 
 import unittest
 
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
-
-from Products.CMFCore.utils import getToolByName
+from plone import api
 
 from canaimagnulinux.web.policy.testing import INTEGRATION_TESTING
 
@@ -22,12 +19,57 @@ class SiteStructureTestCase(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.portal.invokeFactory('Folder', 'test-folder')
-        setRoles(self.portal, TEST_USER_ID, ['Member'])
-        self.folder = self.portal['test-folder']
-        self.folder.invokeFactory('Document', 'd1',)
-        self.d1 = self.folder['d1']
+
+    def test_default_content_is_removed(self):
+        existing = self.portal.objectIds()
+        self.assertTrue('events' not in existing)
+        self.assertTrue('front-page' not in existing)
+        self.assertTrue('news' not in existing)
+
+    def test_root_folders(self):
+        existing = self.portal.objectIds()
+        self.assertIn('canaima', existing)
+        self.assertIn('soluciones', existing)
+        self.assertIn('soporte-y-aprendizaje', existing)
+        self.assertIn('descargas', existing)
+        self.assertIn('comunidad', existing)
+        self.assertIn('novedades', existing)
+        self.assertIn('portada', existing)
+        self.assertEqual(self.portal.getDefaultPage(), 'portada')
+        self.assertIn('Member', existing)
+        member_folder = self.portal['Member']
+        self.assertEquals(self.portal.member_folder.getExcludeFromNav(), True)
+        self.assertIn('pie-de-pagina', existing)
+
+    def test_folder_canaima(self):
+        folder = self.portal['canaima']
+        self.assertEqual(folder.getTitle(), 'Canaima')
+        types = ('Folder', 'Document', 'File', 'Image', 'collective.cover.content', 'CaseStudyFolder',)
+        self.assertEqual(folder.getImmediatelyAddableTypes(), types)
+        self.assertEqual(folder.getLocallyAllowedTypes(), types)
+        self.assertEqual(api.content.get_state(folder), 'published')
+        self.assertIn('conozca-canaima', folder)
+        self.assertIn('caracteristicas', folder)
+        self.assertIn('por-que-usar-canaima', folder)
+        self.assertIn('por-que-software-libre', folder)
+        self.assertIn('casos-de-exitos', folder)
+
+    def test_folder_soluciones(self):
+        folder = self.portal['soluciones']
+        self.assertEqual(folder.getTitle(), 'Soluciones')
+        types = ('Folder', 'Document', 'File', 'Image', 'Collection', 'collective.cover.content',)
+        self.assertEqual(folder.getImmediatelyAddableTypes(), types)
+        self.assertEqual(folder.getLocallyAllowedTypes(), types)
+        self.assertEqual(api.content.get_state(folder), 'published')
+
+    def test_folder_comunidad(self):
+        folder = self.portal['comunidad']
+        self.assertEqual(folder.getTitle(), 'Comunidad')
+        types = ('Folder', 'Document', 'File', 'Image', 'collective.cover.content', 'FSDFacultyStaffDirectory',)
+        self.assertEqual(folder.getImmediatelyAddableTypes(), types)
+        self.assertEqual(folder.getLocallyAllowedTypes(), types)
+        self.assertEqual(api.content.get_state(folder), 'published')
+        self.assertEqual(folder.getLayout(), '@@usersmap_view')
 
 #    def test_portal_title(self):
         
@@ -47,9 +89,9 @@ class SiteStructureTestCase(unittest.TestCase):
         
 #        self.assertEqual("Plone site", self.portal.getProperty('title'))
 
-    def test_able_to_add_document(self):
-        new_id = self.folder.invokeFactory('Document', 'my-page')
-        self.assertEqual('my-page', new_id)
+#    def test_able_to_add_document(self):
+#        new_id = self.folder.invokeFactory('Document', 'my-page')
+#        self.assertEqual('my-page', new_id)
         
     # Keep adding methods here, or break it into multiple classes or
     # multiple files as appropriate. Having tests in multiple files makes

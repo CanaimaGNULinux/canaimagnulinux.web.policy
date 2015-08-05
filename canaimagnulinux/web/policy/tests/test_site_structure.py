e2bdece1d92608dbd7a04e00490f35050317ca08
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-08-05 11:27:21 -0430

    Updated some docstrings, rename some test methods, and remove commeted code

--- canaimagnulinux/web/policy/tests/test_site_structure.py
+++ canaimagnulinux/web/policy/tests/test_site_structure.py
@@ -19,12 +19,14 @@ class SiteStructureTestCase(unittest.TestCase):
         self.member_folder = self.portal['Member']
 
     def test_default_content_is_removed(self):
+        """ This method test that the default content is removed. """
         existing = self.portal.objectIds()
         self.assertTrue('events' not in existing)
         self.assertTrue('front-page' not in existing)
         self.assertTrue('news' not in existing)
 
     def test_root_folders(self):
+        """ This method test if the root folders are existing. """
         existing = self.portal.objectIds()
         self.assertIn('canaima', existing)
         self.assertIn('soluciones', existing)
@@ -38,7 +40,8 @@ class SiteStructureTestCase(unittest.TestCase):
         self.assertEqual(self.member_folder.getExcludeFromNav(), True)
         self.assertIn('pie-de-pagina', existing)
 
-    def test_folder_canaima(self):
+    def test_canaima_folder(self):
+        """ This method test if the items children of canaima folder are existing. """
         folder = self.portal['canaima']
         self.assertEqual(folder.getTitle(), 'Canaima')
         types = ('Folder', 'Document', 'File', 'Image', 'collective.cover.content', 'CaseStudyFolder',)
@@ -51,7 +54,8 @@ class SiteStructureTestCase(unittest.TestCase):
         self.assertIn('por-que-software-libre', folder)
         self.assertIn('casos-de-exitos', folder)
 
-    def test_folder_soluciones(self):
+    def test_soluciones_folder(self):
+        """ This method test if the items children of soluciones folder are existing. """
         folder = self.portal['soluciones']
         self.assertEqual(folder.getTitle(), 'Soluciones')
         types = ('Folder', 'Document', 'File', 'Image', 'Collection', 'collective.cover.content',)
@@ -59,7 +63,8 @@ class SiteStructureTestCase(unittest.TestCase):
         self.assertEqual(folder.getLocallyAllowedTypes(), types)
         self.assertEqual(api.content.get_state(folder), 'published')
 
-    def test_folder_comunidad(self):
+    def test_comunidad_folder(self):
+        """ This method test if the items children of comunidad folder are existing. """
         folder = self.portal['comunidad']
         self.assertEqual(folder.getTitle(), 'Comunidad')
         types = ('Folder', 'Document', 'File', 'Image', 'collective.cover.content', 'FSDFacultyStaffDirectory',)
@@ -67,31 +72,3 @@ class SiteStructureTestCase(unittest.TestCase):
         self.assertEqual(folder.getLocallyAllowedTypes(), types)
         self.assertEqual(api.content.get_state(folder), 'published')
         self.assertEqual(folder.getLayout(), '@@usersmap_view')
-
-    # def test_portal_title(self):
-
-        # This is a simple test. The method needs to start with the name
-        # 'test'.
-
-        # Look at the Python unittest documentation to learn more about hte
-        # kinds of assertion methods which are available.
-
-        # PloneTestCase has some methods and attributes to help with Plone.
-        # Look at the PloneTestCase documentation, but briefly:
-        #
-        #   - self.portal is the portal root
-        #   - self.folder is the current user's folder
-        #   - self.logout() "logs out" so that the user is Anonymous
-        #   - self.setRoles(['Manager', 'Member']) adjusts the roles of the current user
-
-        # self.assertEqual("Plone site", self.portal.getProperty('title'))
-
-    # def test_able_to_add_document(self):
-        # new_id = self.folder.invokeFactory('Document', 'my-page')
-        # self.assertEqual('my-page', new_id)
-
-    # Keep adding methods here, or break it into multiple classes or
-    # multiple files as appropriate. Having tests in multiple files makes
-    # it possible to run tests from just one package:
-    #
-    #   ./bin/test -s canaimagnulinux.web.policy -t test_integration_unit
